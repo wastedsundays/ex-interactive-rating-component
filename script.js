9ebf40e14a2331bde649c037627b09bdf744338b
Adam Hauck <ad.hauck@gmail.com>
2022-08-09 17:12:23 -0700

    update readme, fixed value

--- script.js
+++ script.js
@@ -5,18 +5,14 @@ const ratingSection = document.getElementById("rating-section");
 let userRating = 0;
 
 function displayRadioValue() {
-    var ele = document.getElementsByName('rate');
-      
-    for(i = 0; i < ele.length; i++) {
-        userRating = ele[i].value;
-        //change the message rating number to = the input number
+    //get the value of the selected 'button'
+    userRating = document.querySelector("input[type='radio'][name=rate]:checked").value;
+      //update the response message with the selected value
         document.getElementById('user-rating').innerHTML = userRating;
         //hide the rating 'screen', display the thank you 'screen'
         rating.style.display = "none";
         results.style.display = "block";
     }
-}
-
 
 btn.onclick = displayRadioValue;
 
